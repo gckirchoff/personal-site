c9c4bd4cf31ab2e023b96c224bbc9df8c9e5a3a6
Gregory Kirchoff <gckirchoff@gmail.com>
2021-08-13 10:43:25 -0400

    Added projects section

---
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import './ProjectCard.scss';
+import { FaGithub } from 'react-icons/fa';
+
+const ProjectCard = ({
+  project: {
+    title,
+    image,
+    description,
+    sourceCodeSiteName,
+    sourceCodeSpecificty,
+    sourceCodeLink,
+    siteUrl,
+  },
+}) => {
+  return (
+    <div className="project-card" style={{ backgroundImage: `url(${image})` }}>
+      <div className="card-content">
+        <h2 className="project-title">{title}</h2>
+        <p className="project-description">{description}</p>
+        <div className="project-buttons-container">
+          <a
+            className="project-link-button margin-right-small"
+            href={siteUrl}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Visit
+          </a>
+          {sourceCodeLink.map((link, i) => (
+            <a
+              href={link}
+              target="_blank"
+              rel="noreferrer"
+              className="project-link-button margin-right-small margin-bottom-small"
+            >
+              {sourceCodeSiteName[i] === 'Github' ? <FaGithub /> : ''}
+              {sourceCodeSpecificty[i]}
+            </a>
+          ))}
+          {/* <a
+            className="project-link-button"
+            href={sourceCodeLink}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {sourceCodeSiteName}
+          </a> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -5,8 +5,14 @@ import './MainPage.scss';
 import portrait from '../../assets/gregory-picture.jpg';
 import formalPortrait from '../../assets/gregory-picture-formal.jpeg';
 import bacterifind from '../../assets/bacterifind.png';
+import ecmanoteScreenshot from '../../assets/ecmanote-screenshot.png';
+import catChatScreenshot from '../../assets/cat-chat-screenshot.png';
 import { ReactComponent as BlobSvg } from '../../assets/blob.svg';
 import { ReactComponent as EllipseSvg } from '../../assets/ellipse.svg';
+import ProjectCard from '../../components/ProjectCard/ProjectCard';
+import { FaGithub } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaFilePdf } from 'react-icons/fa';
 
 const mainPageVariants = {
   hidden: {
@@ -16,6 +22,46 @@ const mainPageVariants = {
     opacity: 1,
   },
 };
+
+const projects = [
+  {
+    title: 'Bacterifind',
+    image: bacterifind,
+    description:
+      'A MERN app created with the goal of helping scientists characterize, identify, and research Enterobacteriaceae and other gram negative, oxidase negative bacteria.',
+    sourceCodeSiteName: ['Github', 'Github'],
+    sourceCodeSpecificty: [' Frontend', ' Backend'],
+    sourceCodeLink: [
+      'https://github.com/gckirchoff/bacterifind-frontend',
+      'https://github.com/gckirchoff/bacterifind-backend',
+    ],
+    siteUrl: 'https://fast-temple-25827.herokuapp.com/',
+  },
+  {
+    title: 'Ecmanote',
+    image: ecmanoteScreenshot,
+    description:
+      'A locally hosted React app, served by a CLI. This app allows users to write markdown and compile Javascript that gets run live in the browser as you type, all while writing those changes to the file system. Great for studying data structures and algorithms.',
+    sourceCodeSiteName: ['Github'],
+    sourceCodeSpecificty: [' Source'],
+    sourceCodeLink: ['https://github.com/gckirchoff/ecmanote'],
+    siteUrl: 'https://www.npmjs.com/package/ecmanote',
+  },
+  {
+    title: 'Cat Chat',
+    image: catChatScreenshot,
+    description:
+      'A MERN app created with the goal of helping scientists characterize, identify, and research Enterobacteriaceae and other gram negative, oxidase negative bacteria.',
+    sourceCodeSiteName: ['Github', 'Github', 'Github'],
+    sourceCodeSpecificty: [' Frontend', ' Backend', ' ML API'],
+    sourceCodeLink: [
+      'https://github.com/gckirchoff/cat-chat-client',
+      'https://github.com/gckirchoff/cat-chat-backend',
+      'https://github.com/gckirchoff/cat-chat-flask-api',
+    ],
+    siteUrl: 'https://youthful-raman-950e3d.netlify.app/',
+  },
+];
 
 const MainPage = () => {
   const orbRef = useRef(null);
@@ -83,27 +129,29 @@ const MainPage = () => {
           </ul>
         </div>
       </div>
-      {/* <div className="projects-section">
+      <div className="projects-section">
         <h1>Projects</h1>
         <div className="projects-grid">
-          <div className="project-card">
-            <div className="project-image">
-              <img src={bacterifind} alt="Bacterifind" />
-            </div>
-            <h2 className="project-title">Bacterifind</h2>
-            <div>
-              <p>This project is a project</p>
-            </div>
-            <div className="tech-used">
-              <p>React</p>
-              <p>Redux</p>
-              <p>Nodejs</p>
-              <p>Express</p>
-              <p>MongoDB</p>
-            </div>
-          </div>
+          {projects.map((project) => (
+            <ProjectCard project={project} />
+          ))}
         </div>
-      </div> */}
+      </div>
+      <footer className="contact">
+        <h1>Contact</h1>
+        <h3 className="email">gckirchoff@gmail.com</h3>
+        <div className="contact-links-container">
+          <a href="#">
+            <FaLinkedin /> LinkedIn
+          </a>
+          <a href="#">
+            <FaGithub /> Github
+          </a>
+          <a href="#">
+            <FaFilePdf /> Resume
+          </a>
+        </div>
+      </footer>
     </motion.div>
   );
 };
